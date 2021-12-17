4589b19213249bdf6e815728586b523854b0d23d
jimschenchen <jimschenchen@gmail.com>
2021-12-17 02:21:24 -0500

    Update

--- site.config.js
+++ site.config.js
@@ -7,19 +7,19 @@ module.exports = {
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'jimschenchen',
+  name: 'ProbeJFury Documentation',
   domain: 'jimschenchen.tech',
-  author: 'jimschenchen',
+  author: 'ProbeJFury',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  // description: 'Example site description',
+  // socialImageTitle: 'Transitive Bullshit',
+  // socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  // twitter: 'transitive_bs',
+  // github: 'transitive-bullshit',
+  // linkedin: 'fisch2',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
